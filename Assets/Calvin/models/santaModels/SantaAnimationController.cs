d1cc608b3d2ffdb308ed9a8da3926dff52e56bfc
kumagai2 <kumagai2@umbc.edu>
2021-01-30 21:54:39 -0500

    Made Santa Prefab.

---
+++ Assets/Calvin/models/santaModels/SantaAnimationController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaAnimationController : MonoBehaviour
+{
+
+    public enum SantaState { happy, angry, attack};
+    public SantaState currentState;
+
+    public List<SantaStateGroup> santaStateGroups;
+
+    [System.Serializable]
+    public class SantaStateGroup
+    {
+        public GameObject santaObject;
+        public SantaState state;
+
+        public SantaStateGroup(GameObject santaObj, SantaState santaSt)
+        {
+            santaObject = santaObj;
+            state = santaSt;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(SantaStateGroup st in santaStateGroups)
+        {
+            if(st.state != currentState)
+            {
+                st.santaObject.SetActive(false);
+            }
+            else
+            {
+                st.santaObject.SetActive(true);
+            }
+        }
+    }
+}
